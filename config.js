d814251a8a1ca96c79bc7bbeac8a94b0cd61c783
rodrigosinistro <80776011+rodrigosinistro@users.noreply.github.com>
2026-04-01 23:08:16 -0300

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://lvmnwvxdjknfcbiypwpd.supabase.co',
-  SUPABASE_ANON_KEY: 'COLE_AQUI_SUA_SUPABASE_ANON_KEY',
+  SUPABASE_ANON_KEY: 'sb_publishable_Z3lermGUqlkl528bF89B_g_9I7sRhI2',
   APP_NAME: 'Custos de Impressão 3D',
   REPOSITORY_URL: 'https://github.com/rodrigosinistro/custos-impressao-3d',
   PAGES_URL: 'https://rodrigosinistro.github.io/custos-impressao-3d/'
