0131353e1ee23b198b8bd928fb1d7a9e7c17d3ad
rodrigosinistro <80776011+rodrigosinistro@users.noreply.github.com>
2026-04-01 23:06:37 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: 'COLE_AQUI_SEU_SUPABASE_URL',
+  SUPABASE_URL: 'postgresql://postgres:Rod@301613@db.lvmnwvxdjknfcbiypwpd.supabase.co:5432/postgres',
   SUPABASE_ANON_KEY: 'COLE_AQUI_SUA_SUPABASE_ANON_KEY',
   APP_NAME: 'Custos de Impressão 3D',
   REPOSITORY_URL: 'https://github.com/rodrigosinistro/custos-impressao-3d',
